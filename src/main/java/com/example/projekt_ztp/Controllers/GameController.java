35ee3c2cd14a4913f0363cef0a3338548b8d3774
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2024-01-16 00:48:54 +0100

    basic ship movement

--- src/main/java/com/example/projekt_ztp/Controllers/GameController.java
+++ src/main/java/com/example/projekt_ztp/Controllers/GameController.java
@@ -5,7 +5,11 @@ import com.example.projekt_ztp.StageProperties;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +18,32 @@ public class GameController {
     @FXML
     private AnchorPane anchorPane;
     @FXML
+    private BorderPane borderPane;
+    @FXML
+    private Button ship;
+    @FXML
+    private Label title;
+    @FXML
     private void initialize() {
+        Image shipImage = new Image("file:src/main/resources/com/example/projekt_ztp/Images/ship.png");
+        String shipStyle = "-fx-background-image: url('" + shipImage.getUrl() + "'); " +
+                "-fx-background-size: cover;";
+
+        ship.setStyle(shipStyle);
+
+        //-100, ponieważ trzeba odjąć grubość dwóch ramek po 10 i lewego paddingu 80
+        ship.setLayoutX((StageProperties.STAGE_WIDTH-ship.getMinWidth())/2-100);
+
+        ship.setOnKeyPressed(event -> {
+            double x = ship.getLayoutX();
+            switch (event.getCode()) {
+                case LEFT -> x-=10;
+                case RIGHT -> x+=10;
+            }
+            if(x>40 && x<StageProperties.STAGE_WIDTH-295) {
+                ship.setLayoutX(x);
+            }
+        });
     }
     @FXML
     private void backToMenu() throws IOException {
