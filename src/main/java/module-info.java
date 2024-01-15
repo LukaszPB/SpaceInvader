a7075c27256d2e74673e0987cb4b608280490813
LukaszPB <lukasz.kiersnowski.110752@student.pb.edu.pl>
2024-01-15 15:14:17 +0100

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.projekt_ztp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.projekt_ztp to javafx.fxml;
+    exports com.example.projekt_ztp;
+}
